43f057cf75379182ee5e6627d6de8ac219c85ace
KevzPeter <ultimatekevin1@gmail.com>
2024-04-20 01:43:40 +0530

    Updated Header and Footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 const Header = () => {
     return (
-        <header className="container px-8 m-0">
+        <header className="mx-8 my-2 flex">
             <div className="flex h-20 w-full items-center">
                 <Link href="/">
-                    <span className="text-2xl text-gray-300 font-medium">PS5 Controller Tester</span>
+                    <span className="text-2xl text-gray-200 font-medium">PS5 Controller Tester</span>
                 </Link>
             </div>
         </header>
