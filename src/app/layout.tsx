2357c9fe721ebf67187f805bbcfffa99f89312aa
KevzPeter <ultimatekevin1@gmail.com>
2024-04-20 01:44:00 +0530

    Fixed layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
 import { cn } from "@/lib/utils"
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,9 +23,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn(
-        "min-h-screen bg-background font-sans antialiased",
+        "min-h-screen bg-slate-900 font-sans antialiased !p-0 !m-0",
         inter.variable
-      )}>{children}</body>
+      )}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
