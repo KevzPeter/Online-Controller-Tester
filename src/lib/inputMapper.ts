92a99fea150da36f362ba8c9a67b9b1d0eae6270
KevzPeter <ultimatekevin1@gmail.com>
2024-04-20 00:55:35 +0530

    added degreesToRadians and rounding function

--- src/lib/inputMapper.ts
+++ src/lib/inputMapper.ts
@@ -1,3 +1,11 @@
-export const convertAnalogToSlider = (analogValue: number) => {
+export const convertAnalogToSlider = (analogValue: number): number => {
     return (analogValue + 1) * 50;
+}
+
+export const degreesToRadians = (degrees: number): number => {
+    return degrees * (Math.PI / 180);
+}
+
+export const roundToOneDecimalPlace = (number: number): number => {
+    return Math.round(number * 10) / 10;
 }
