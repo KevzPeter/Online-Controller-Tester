f3971f265692ab915094f94fc4bdc2d807fc148f
KevzPeter <ultimatekevin1@gmail.com>
2024-04-20 23:50:24 +0530

    Added loading screen for model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import GamePad from "@/components/GamePad";
 import CanvasContainer from "@/components/Canvas";
-
+import Spinner from "@/components/Spinner";
+import { Suspense } from "react";
 export default function Home() {
   return (
     <main className="flex flex-col lg:flex-row text-gray-400">
       <GamePad />
-      <CanvasContainer />
+      <Suspense fallback={<Spinner />}>
+        <CanvasContainer />
+      </Suspense>
     </main>
   );
 }
--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,28 +1,40 @@
 "use client";
-import { useState, Suspense, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useFrame } from "@react-three/fiber";
 import { Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import PS5Controller from "../../public/Ps5-controller";
+import XboxController from "../../public/Xbox-controller";
 import { degreesToRadians, roundToOneDecimalPlace } from "@/lib/inputMapper";
 
 const SceneContainer = () => {
 
     const [scaleFactor, setScaleFactor] = useState(0.9);
+    const [controllerType, setControllerType] = useState("");
 
     const orbitControlsRef: any = useRef(null);
+
     useFrame(() => {
         if (orbitControlsRef.current) {
             const controller = navigator.getGamepads()[0];
             if (controller) {
                 const x = controller.axes[2];
                 const y = controller.axes[3];
-                if (controller.buttons[6].pressed) {
+                if (controller.buttons[6]?.pressed) {
                     setScaleFactor(Math.max(0.5, scaleFactor - 0.005));
-                    console.log(scaleFactor);
                 }
-                if (controller.buttons[7].pressed) {
+                if (controller.buttons[7]?.pressed) {
                     setScaleFactor(Math.min(0.9, scaleFactor + 0.005));
-                    console.log(scaleFactor);
+                }
+                if (controller.id) {
+                    if (controller.id.toLowerCase().startsWith("xbox")) {
+                        setControllerType("xbox")
+                    }
+                    else if (controller.id.toLowerCase().startsWith("dualsense")) {
+                        setControllerType("dualsense")
+                    }
+                    else {
+                        setControllerType("");
+                    }
                 }
                 orbitControlsRef.current.setAzimuthalAngle(-x * degreesToRadians(180));
                 orbitControlsRef.current.setPolarAngle((y + 1) * degreesToRadians(90));
@@ -32,20 +44,29 @@ const SceneContainer = () => {
     })
 
     useEffect(() => {
-        if (orbitControlsRef.current) {
-            console.log(orbitControlsRef.current);
-        }
     }, [orbitControlsRef.current]);
+
+    const RenderController = () => {
+        if (controllerType === 'xbox') {
+            return (
+                <XboxController scaleFactor={scaleFactor} />
+            )
+        }
+        else if (controllerType === 'dualsense') {
+            return (
+                <PS5Controller scaleFactor={scaleFactor} />
+            )
+        }
+        else return null;
+    }
 
     return (
         <>
-            <PerspectiveCamera makeDefault position={[0, 0, 2.5]} />
+            <PerspectiveCamera makeDefault position={[0, 0, (controllerType ? (controllerType === "xbox" ? 20 : 2.5) : 0)]} />
             <ambientLight />
             <OrbitControls ref={orbitControlsRef} minPolarAngle={degreesToRadians(0)} maxPolarAngle={degreesToRadians(180)} enablePan={false} enableRotate={false} enableZoom={false} />
-            <Suspense fallback={null}>
-                <PS5Controller scaleFactor={scaleFactor} />
-                <Environment preset="sunset" />
-            </Suspense>
+            <RenderController />
+            <Environment preset="sunset" />
         </>
     )
 }
