f0e27a183c71d336675e765681198b0e4808eb94
KevzPeter <ultimatekevin1@gmail.com>
2025-09-21 20:44:46 -0400

    Added Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "../styles/globals.css";
 import { cn } from "@/lib/utils"
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -29,6 +30,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
